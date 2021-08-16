0eb0e99a24e90bd9d8a940090f11a70c277a4eea
Gamestester <Gamestester@DEL-PC-0224.(none)>
2021-08-16 23:13:50 +0530

    final commit

--- src/Components/BSTC.js
+++ src/Components/BSTC.js
@@ -7,73 +7,171 @@ const BSTC = () => {
             <h1 className='bstc-head'>Babel Software Testing Certification Champions</h1>
             <h3 className='bstc-intro'>There are always new, grander challenges to confront, and a true winner will embrace each one.</h3>
             <Container fluid>
-                <Row className='bstc-winner'>
-                    <Col>
-                        <img alt='Achint' className='winner-img' src={ require("../Images/BSTC/Swastik.png").default } />
-                        <h3 className='bstc-name'>Swastik Dhamija</h3>
-                        <h5>Marks : 40</h5>
-                    </Col>
-                    <Col>
-                        <img alt='Pushpankar' className='winner-img' src={require('../Images/BSTC/Pushpankar.png').default} />
-                        <h3 className='bstc-name'>Pushpankar Kiran</h3>
-                        <h5>Marks : 39</h5>
-                    </Col>
-                    <Col>
-                        <img alt='Aastha' className='winner-img' src={require('../Images/BSTC/Aastha.png').default} />
-                        <h3 className='bstc-name'>Aastha Dixit</h3>
-                        <h5>Marks : 39</h5>
-                    </Col>
-                    <Col>
-                        <img alt='Mukul' className='winner-img' src={require('../Images/BSTC/Mukul.png').default} />
-                        <h3 className='bstc-name'>Mukul Singh</h3>
-                        <h5>Marks : 37</h5>
-                    </Col>
-                </Row>
-                <Row className='bstc-winner'>
-                    
-                    <Col>
-                        <img alt='Upasana' className='winner-img' src={require('../Images/BSTC/Upasana.png').default} />
-                        <h3 className='bstc-name'>Upasana Makkar</h3>
-                        <h5>Marks : 36</h5>
-                    </Col>
-                    <Col>
-                        <img alt='Giridhar' className='winner-img' src={require('../Images/BSTC/Giridhar.png').default} />
-                        <h3 className='bstc-name'>Giridhar Raut</h3>
-                        <h5>Marks : 35</h5>
-                    </Col>
-                    <Col>
-                        <img alt='Shashi' className='winner-img' src={require('../Images/BSTC/Shashi.png').default} />
-                        <h3 className='bstc-name'>Shashi Sharma</h3>
-                        <h5>Marks : 35</h5>
-                    </Col>
-                    <Col>
-                        <img alt='Rishab' className='winner-img' src={require('../Images/BSTC/Rishab.png').default} />
-                        <h3 className='bstc-name'>Rishabh Kaushik</h3>
-                        <h5>Marks : 35</h5>
-                    </Col>
-                </Row>
-                <Row className='bstc-winner'>
-                    <Col>
-                        <img alt='Aman' className='winner-img' src={require('../Images/BSTC/Aman.png').default} />
-                        <h3 className='bstc-name'>Aman Kumar Mishra</h3>
-                        <h5>Marks : 34</h5>
-                    </Col>
-                    <Col>
-                        <img alt='Rahul' className='winner-img' src={require('../Images/BSTC/Rahul.png').default} />
-                        <h3 className='bstc-name'>Rahul Bisht</h3>
-                        <h5>Marks : 33</h5>
-                    </Col>
-                    <Col>
-                        <img alt='Vincent' className='winner-img' src={require('../Images/BSTC/Vincent.png').default} />
-                        <h3 className='bstc-name'>Vincent Tudu</h3>
-                        <h5>Marks : 33</h5>
-                    </Col>
-                    <Col>
-                        
-                        <h3 className='bstc-name'></h3>
-                        <h5></h5>
-                    </Col>
-                </Row>
+               <h1 className='bstc-name'>June</h1>
+
+               <table>
+                   <tr>
+                       <th>Name</th>
+                       <th>Marks</th>
+                       <th>Name</th>
+                       <th>Marks</th>
+                   </tr>
+                   <tr>
+                       <th>Swastik  Dhamija</th>
+                       <th>40</th>
+                       <th>Pushpankar  Kiran</th>
+                       <th>39</th>
+                   </tr>
+                   <tr>
+                       <th>Aastha  Dixit</th>
+                       <th>39</th>
+                       <th>Saransh  Shekhar</th>
+                       <th>38</th>
+                   </tr>
+                   <tr>
+                       <th>Mukul  Singh</th>
+                       <th>37</th>
+                       <th>Piyush  Kaushik</th>
+                       <th>37</th>
+                   </tr>
+                   <tr>
+                       <th>Akashdeep  Singh</th>
+                       <th>36</th>
+                       <th>Karanveer  Singh</th>
+                       <th>36</th>
+                   </tr>
+                   <tr>
+                       <th>Shaurya  Chhetri</th>
+                       <th>36</th>
+                       <th>Upasana  Makkar</th>
+                       <th>36</th>
+                   </tr>
+                   <tr>
+                       <th>Srajan  Bhardwaj</th>
+                       <th>35</th>
+                       <th>Harshit  Gupta</th>
+                       <th>35</th>
+                   </tr>
+                   <tr>
+                       <th>Vimmy  Vashisht</th>
+                       <th>35</th>
+                       <th>Nirdosh  Kumar</th>
+                       <th>35</th>
+                   </tr>
+                   <tr>
+                       <th>Giridhar  Raut</th>
+                       <th>35</th>
+                       <th>Shashi  Sharma</th>
+                       <th>35</th>
+                   </tr>
+                   <tr>
+                       <th>Inder  Parhar</th>
+                       <th>35</th>
+                       <th>Ankit  Kumar</th>
+                       <th>35</th>
+                   </tr>
+                   <tr>
+                       <th>Akhilesh Kumar  Chandel</th>
+                       <th>35</th>
+                       <th>Rishabh  Kaushik</th>
+                       <th>35</th>
+                   </tr>
+                   <tr>
+                       <th>Achint  Razdan</th>
+                       <th>34</th>
+                       <th>Manish  Kundara</th>
+                       <th>34</th>
+                   </tr>
+                   <tr>
+                       <th>Aman Kumar  Mishra</th>
+                       <th>34</th>
+                       <th>Rahul  Bisht</th>
+                       <th>33</th>
+                   </tr>
+                   <tr>
+                       <th>Vincent  Tudu</th>
+                       <th>33</th>
+                       <th>Himanshu  </th>
+                       <th>32</th>
+                   </tr>
+                   <tr>
+                       <th>Mayank  Singh</th>
+                       <th>32</th>
+                       <th>Krishna  Bajaj</th>
+                       <th>32</th>
+                   </tr>
+               </table>
+               <h1 className='bstc-name'>July</h1>
+
+               <table>
+    <tr>
+        <th>Name</th>
+        <th>Marks</th>
+        <th>Name</th>
+        <th>Marks</th>
+    </tr>
+    <tr>
+        <th>Prachi Malhotra</th>
+        <th>40</th>
+        <th>Tushar Mathur</th>
+        <th>39</th>
+    </tr>
+    <tr>
+        <th>Nikhil Varghese</th>
+        <th>38</th>
+        <th>Hemant Kumar</th>
+        <th>38</th>
+    </tr>
+    <tr>
+        <th>Vaishnavi Maroo</th>
+        <th>38</th>
+        <th>Siddharth Pandey</th>
+        <th>37</th>
+    </tr>
+    <tr>
+        <th>Nishant Sheoran</th>
+        <th>37</th>
+        <th>Rahat Pal</th>
+        <th>36</th>
+    </tr>
+    <tr>
+        <th>Rishav Kumar</th>
+        <th>36</th>
+        <th>Devesh Kumar</th>
+        <th>36</th>
+    </tr>
+    <tr>
+        <th>Tushar Choudhary</th>
+        <th>35</th>
+        <th>Jatin Chawla</th>
+        <th>34</th>
+    </tr>
+    <tr>
+        <th>Nabhdeep Choudhary</th>
+        <th>34</th>
+        <th>Sangay Jambel</th>
+        <th>34</th>
+    </tr>
+    <tr>
+        <th>Paras Verma</th>
+        <th>33</th>
+        <th>Rahul Ahluwalia</th>
+        <th>33</th>
+    </tr>
+    <tr>
+        <th>Lalit Aswal</th>
+        <th>33</th>
+        <th>Anurag Kumar</th>
+        <th>33</th>
+    </tr>
+    <tr>
+        <th>Pritish Raj</th>
+        <th>32</th>
+        <th>Abhishek Uppal</th>
+        <th>32</th>
+    </tr>
+        
+</table>
             </Container>
         </div>
   );
--- src/Components/Cricket.js
+++ src/Components/Cricket.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Container} from "react-bootstrap"
 import "./Styles/Cricket.css"
+import ListGroup from 'react-bootstrap/ListGroup'
+import Image from 'react-bootstrap/Image'
+import { Row,Col } from "react-bootstrap";
+import "./Styles/Games.css"
 
 function Cricket() {
     return(
@@ -10,11 +14,26 @@ function Cricket() {
             <Container fluid>
                 <div className="match">
                     <h3 className='match-highlights'>Cricket Match Highlights</h3>
-                    <ul className="list">
-                        <li>In first match Vinod Pati won the toss and asked Akshat Tripathi to bat first.</li>
-                        <li>With great batting show from Akash Varshney (83 in 46 balls) and small contribution from Akshat, Harpal and Rahul Solanki, batting team scored 201/5 in 20 overs.</li>
-                        <li>Chasing a target of more than 200 was not to be very easy for the chasing team and opening spell from Rahul Solanki and off spin from Harshit (4 wickets) made it nearly impossible task.</li>
-                    </ul>
+                   
+                    
+                    <Container>
+                    <ListGroup variant="flush" >
+                            
+                             </ListGroup>
+                             <ListGroup.Item ClassN>In first match Vinod Pati won the toss and asked Akshat Tripathi to bat first. 
+                                </ListGroup.Item>
+                                <ListGroup.Item>With great batting show from Akash Varshney (83 in 46 balls) and small contribution from Akshat, Harpal and Rahul Solanki, batting team scored 201/5 in 20 overs.</ListGroup.Item>
+                                <ListGroup.Item>Chasing a target of more than 200 was not to be very easy for the chasing team and opening spell from Rahul Solanki and off spin from Harshit (4 wickets) made it nearly impossible task.
+                                </ListGroup.Item>  
+                                <Image  className="sports_image"   src={ require("../Images/Games/Scoreboard.png").default }   />             
+                                <Container>
+                                
+                                 </Container>
+                               </Container>
+                   
+                    
+
+                    
                 </div>
             </Container>
         </div>
